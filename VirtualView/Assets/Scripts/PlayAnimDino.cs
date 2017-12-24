57f1d05b602b2bda76a6a14694c7b83389e56e9c
Paweł Brzoza <brzozik95@gmail.com>
2017-12-24 16:26:36 +0100

    Allosaurus added

---
+++ VirtualView/Assets/Scripts/PlayAnimDino.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class PlayAnimDino : MonoBehaviour {
+
+	public Animation anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void animIdle(){
+		anim.Play ("idle");
+	}
+
+	public void animHit(){
+		anim.Play ("hit");
+	}
+
+}
